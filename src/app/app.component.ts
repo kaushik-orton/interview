0e6dfeab087411635080e07cd3a9cc1065d91831
kaushik-orton <nittalakaushik98@gmail.com>
2021-10-23 15:27:04 +0530

    'currecny'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Auth0';
+  title = 'Test';
+  data:any
+  products:any
+  usd:number =0
+  url='https://v6.exchangerate-api.com/v6/d659966f6449a96c9309fa45/latest/inr'
+  url1="https://my-json-server.typicode.com/kaushik-orton/Jsondata/data"
+  flag:boolean = true;
+  constructor(private http:HttpClient){
+    http.get(this.url).subscribe(p=>{this.data=p;
+     this. usd=this.data.conversion_rates.USD
+    })
+
+    http.get(this.url1).subscribe(p=>{this.products=p})
+  }
+  iff(){
+    this.flag=!this.flag
+    console.log('value changed',this.flag)
+  }
 }
